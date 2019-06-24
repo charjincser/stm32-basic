d4eac2d065fe3f5e767a0e1dc7dfd4091f2ec6ff
Char Jin <charjincser@gmail.com>
2019-06-24 20:54:02 +0800

    Fix the error about the defination of led funcs.

--- bpdriver/led.c
+++ bpdriver/led.c
@@ -10,29 +10,6 @@ void led_init(void)
 	GPIO_Init(led_gpio, &GPIO_InitStructure);
 }
 
-/**
- * 根据下标决定点亮哪一led灯
- */
-void led_on(int index)
-{
-	switch (index)
-	{
-		case 1 : GPIO_SetBits(led_gpio, 0x0040);break;	// 0000 0000 0100 0000
-		case 2 : GPIO_SetBits(led_gpio, 0x0080);break;	// 0000 0000 1000 0000
-		case 3 : GPIO_SetBits(led_gpio, 0x0100);break;	// 0000 0001 0000 0000
-		case 4 : GPIO_SetBits(led_gpio, 0x0200);break;	// 0000 0010 0000 0000
-		default: break;
-	}
-}
-
-/**
- * 熄灭所有led灯
- */
-void led_all_off(void)
-{
-	GPIO_SetBits(led_gpio, 0x0000);
-}
-
 // void beep_run(INT8U runtime)
 // {
 // 	GPIO_SetBits(beep_gpio, beep);
